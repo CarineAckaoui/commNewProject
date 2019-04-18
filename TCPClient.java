16c4146d4f5a106f7cf3c7eaba1a812c139a1edd
CarineAckaoui <49471565+CarineAckaoui@users.noreply.github.com>
2019-04-18 12:04:45 +0200

    Add files via upload

--- TCPClient.java
+++ TCPClient.java
@@ -1,36 +1,43 @@
+import java.io.*;
+import java.net.*;
 
-import java.io.*; 
-	import java.net.*; 
-	class TCPClient { 
+class TCPClient {
 
-	    public static void main(String argv[]) throws Exception 
-	    { 
-	        String sentence; 
-	        String modifiedSentence; 
+	public static void main(String argv[]) throws Exception {
+		String sentence;
+		String modifiedSentence;
+		
 
-	        BufferedReader inFromUser = 
-	          new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
+				System.in));
+		sentence = inFromUser.readLine();
 
-	        Socket clientSocket = new Socket("127.0.0.1", 6789); 
+		if (sentence.equals("CONNECT")) {
+			System.out.println("SYSTEM CONNECTED");
+			while (true) {
+				Socket clientSocket = new Socket("172.20.10.13", 6789);
+				System.out.println("please type message:" + '\n');
+				BufferedReader inFromUser2 = new BufferedReader(
+						new InputStreamReader(System.in));
+				BufferedReader inFromServer = new BufferedReader(
+						new InputStreamReader(clientSocket.getInputStream()));
+				DataOutputStream outToServer = new DataOutputStream(
+						clientSocket.getOutputStream());
 
-	        DataOutputStream outToServer = 
-	          new DataOutputStream(clientSocket.getOutputStream());
-	        BufferedReader inFromServer = 
-	                new BufferedReader(new
-	                InputStreamReader(clientSocket.getInputStream())); 
-
-	              sentence = inFromUser.readLine(); 
-
-	              outToServer.writeBytes(sentence + '\n'); 
-
-	              modifiedSentence = inFromServer.readLine(); 
-
-	              System.out.println("FROM SERVER: " + modifiedSentence); 
-
-	              clientSocket.close(); 
-	                         
-	          } 
-	      } 
-
-
+				sentence = inFromUser2.readLine();
+				if (sentence.equals("CLOSE")) {
+					System.out.println("Server is closed");
+					clientSocket.close();
+					return;
+				}
+				else {
+						outToServer.writeBytes(sentence + '\n');
+						outToServer.flush();
+						modifiedSentence = inFromServer.readLine();
+						System.out.println("Client 2: " + modifiedSentence);
+					}
+				}
+			}
+		}
+	}
 
--- TCPServer.java
+++ TCPServer.java
@@ -7,10 +7,10 @@ class TCPServer {
   public static void main(String argv[]) throws Exception 
     { 
       String clientSentence; 
-      String capitalizedSentence; 
+      String user2; 
 
       ServerSocket welcomeSocket = new ServerSocket(6789); 
-  
+
       while(true) { 
   
             Socket connectionSocket = welcomeSocket.accept(); 
@@ -22,11 +22,15 @@ class TCPServer {
                    new DataOutputStream(connectionSocket.getOutputStream()); 
 
                  clientSentence = inFromClient.readLine(); 
-
-                 capitalizedSentence = clientSentence.toUpperCase() + '\n'; 
-
-                 outToClient.writeBytes(capitalizedSentence);
-        
+                 if(clientSentence!= null){
+                 System.out.println("Client 1: "+ clientSentence );
+                 System.out.println("please type repley:");
+                 BufferedReader client2 = new BufferedReader(
+ 						new InputStreamReader(System.in));
+                 user2= client2.readLine();
+                 outToClient.writeBytes(user2+'\n');
+                 outToClient.flush();
+                 }       
               } 
       
           } 
